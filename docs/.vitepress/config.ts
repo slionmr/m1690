207ff655497b2e09e1ec121e1f0695a2da0349fb
slionmr <slionmr@yahoo.com>
2024-04-22 19:11:43 +0300

    start

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -7,7 +7,6 @@ import { head, nav, sidebar } from './configs'
 const APP_BASE_PATH = basename(process.env.GITHUB_REPOSITORY || '')
 
 export default defineConfig({
-  outDir: '../dist',
   base: '/',
 
   lang: 'ru-RU',
